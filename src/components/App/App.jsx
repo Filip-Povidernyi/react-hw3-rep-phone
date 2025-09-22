d733eb0a39841bbab821a9ca9af993e0f1dd433c
Filip-Povidernyi <p.poviderniy@gmail.com>
2025-09-22 10:13:40 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,10 +10,10 @@ class App extends Component {
 
   state = {
     contacts: [
-      {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-      {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-      {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-      {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
     ],
     filter: ''
   };
@@ -22,14 +22,29 @@ class App extends Component {
     this.setState({ filter: evt.target.value });
   };
 
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    if (contacts) {
+      this.setState(JSON.parse(contacts));
+    };
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state));
+    };
+  };
+
   addContact = (name, number) => {
     const { contacts } = this.state;
     if (contacts.map(contact => contact.name.toLowerCase()).includes(name.toLowerCase())) {
       alert(`${name} is already in contacts.`);
       return
     }
-    const newContact = { id: nanoid(), name: name.toLowerCase(), number }
-    this.setState({ contacts: [...contacts, newContact] });
+    const newContact = { id: nanoid(), name: name.toLowerCase(), number };
+    const contactsObj = { contacts: [...contacts, newContact] };
+    this.setState(contactsObj);
+    localStorage.setItem('contacts', JSON.stringify(contactsObj));
   };
 
   deleteContact = (name) => {
